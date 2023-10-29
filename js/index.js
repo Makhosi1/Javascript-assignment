8fdd5e6edbc00f50951bd6bdb60ba460b882c8eb
Coco <admin@Makhosi-MacBook-Pro.local>
2023-10-29 14:36:58 +0000

    display resuls

--- js/index.js
+++ js/index.js
@@ -16,34 +16,52 @@ const questions = [
         question: "Who is Beyonce?",
         possAnswers: ['Prime Minister', 'Artist', 'IG Influencer', 'Pastor'],
         answer: "Artist",
-        name:"beyonceq"
+        name: "beyonceq"
     }
 ];
+
+
 
 const quizContainer = document.querySelector(".quiz-container");
 const quiz = document.querySelector("#quiz");
 const submitbtn = document.querySelector("#submit");
 const results = document.querySelector("#results");
 
+const header = document.querySelector("#header");
+header.innerHTML= "Simple Quiz";
+
 let score = 0;
-let incorrectAnswer =[];
+let incorrectAnswer = [];
+let question = 0;
 
 function buildQuiz() {
+   
 
     function showquestions() {
+        /*const questionInfo = questions[question];
+        const questionElement = document.createElement('div');
+        questionElement.className = 'questionDiv';
+        questionElement.innerHTML = questionInfo.question;
+    
+        const optionsElement = document.createElement('div');
+        optionsElement.className = 'options';*/
+
         for (let quest = 0; quest < questions.length; quest++) {
 
-            const question = document.createElement('h2');
-            console.log(questions[quest]);
+            const quizQuestion = document.createElement('h2');
+            quizQuestion.textContent = questions[quest].question;
+            console.log(quizQuestion);
+           
+            quiz.appendChild(quizQuestion);
+           
+           
 
-            question.textContent = questions[quest].question;
-            quiz.appendChild(question);
 
 
             for (let posAns = 0; posAns < questions[quest].possAnswers.length; posAns++) {
                 const option = document.createElement("label");
                 option.textContent = questions[quest].possAnswers[posAns];
-                option.className = 'options';
+                option.className = 'quizOptions';
 
                 const input = document.createElement("input");
                 input.type = "radio";
@@ -52,7 +70,9 @@ function buildQuiz() {
 
                 option.appendChild(input);
                 quiz.appendChild(option);
+
             }
+           
         }
     }
 
@@ -61,31 +81,35 @@ function buildQuiz() {
         questions.forEach(question => {
             let answer = ""
             const selectedAnswer = document.querySelector(`input[name="quiz-${question.name}"]:checked`);
-            if (selectedAnswer) { 
+            if (selectedAnswer) {
                 answer = selectedAnswer.value;
                 if (answer === question.answer) {
-                    score ++;
+                    score++;
                     return;
-                }    
-            } 
+                }
+            }
 
             incorrectAnswer.push({
                 question: question.question,
-                incorrectAnswer : answer, 
+                incorrectAnswer: answer,
                 correctAnswer: question.answer,
             });
         });
         // remove console log stuff 
-       console.log(incorrectAnswer);
-    
+        console.log(incorrectAnswer);
+
     }
 
 
     showquestions();
 
-    submitbtn.onclick = function () {
+    submitbtn.onclick = function displayResults (){
         showresults();
+        results.innerHTML = `You scored ${score} out of ${questions.length}!`;
     }
+    submitbtn.addEventListener('click', function(){
+        document.h2.style.backgroundColor = 'green';
+    })
 
 };
 
